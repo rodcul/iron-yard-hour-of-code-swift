91f0d5a172562fa8561c157d436e17c3e2f44e57
Rodney Cullen <rodcul@gmail.com>
2015-12-08 14:10:39 +0000

    Initial Commit

---
+++ Iron Yard hour of code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Iron Yard hour of code
+//
+//  Created by Rodney Cullen on 08/12/15.
+//  Copyright © 2015 Optilo Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
